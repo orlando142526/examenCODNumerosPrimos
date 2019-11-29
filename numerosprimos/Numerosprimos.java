32fef1a8454e76c8271791ebc8f630609d8c8a35
DANIELCASTELAO\oyagualendara <oyagualendara@daniecastelao.org>
2019-11-29 14:17:05 +0100

    refactorizacion

--- numerosprimos/Numerosprimos.java
+++ numerosprimos/Numerosprimos.java
@@ -5,55 +5,59 @@ public class Numerosprimos {
     public static boolean p = false;
 
     public static void main(String arg[]) {
-        int nD = 0;
-        int nd = 0;
-        nD = Integer.parseInt(arg[0]);
-        if (nD <= 0) {
+        int numeroDigitos = 0;
+        int numeroVecesDigitos = 0;
+        numeroDigitos = Integer.parseInt(arg[0]);
+        if (numeroDigitos <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
-        for (int i = 1; i <= 99999; i++) {
-            int div = i;
-            int c = 0;
+        for (int valorDivision = 1; valorDivision <= 99999; valorDivision++) {
+            int divisionEntera = valorDivision;
 
-            while (div != 0) {
-                div = div / 10;
-                c++;
+            int contador = 0;
+
+            while (divisionEntera != 0) {
+                divisionEntera = divisionEntera / 10;
+                contador++;
             }
-            nd = c;
+            numeroVecesDigitos = contador;
 
-            if (nd == nD) {
-                if (i < 4) {
+            if (numeroVecesDigitos == numeroDigitos) {
+                if (valorDivision < 4) {
                     p = true;
                 } else {
-                    if (i % 2 == 0) {
+                    if (valorDivision % 2 == 0) {
                         p = false;
                     } else {
-                        int c1 = 0;
+                        int contador1 = 0;
                         int i1 = 1;
-                        int l = (i - 1) / 2;
-                        if (l % 2 == 0) {
-                            l--;
+                        int limite = (valorDivision - 1) / 2;
+                        if (limite % 2 == 0) {
+                            limite--;
                         }
-                        while (i1 <= l) {
-                            if (i % i1 == 0) {
-                                c1++;
+
+                        while (i1 <= limite) {
+                            if (valorDivision % i1 == 0) {
+                                contador1++;
                             }
                             i1 += 2;
-                            if (c1 == 2) {
-                                i1 = l + 1;
+                            if (contador1 == 2) {
+                                i1 = limite + 1;
                             }
                         }
 
-                        if (c1 == 1) {
+                        if (contador1 == 1) {
                             p = true;
                         }
                     }
                 }
 
                 if (p == true) {
-                    System.out.println(i);
+                    System.out.println(valorDivision);
                 }
             }
         }
     }
+
 }
+//refactorizacion en rename y cambiamos los nombres nD=numeroDigitos, nd=numeroVecesDigitos, i=valorDivision , div= divisionEntera, c=contador, c1=limite
